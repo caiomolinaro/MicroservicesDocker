89fab5fc5b3f5ee9996117d4409f16364044b501
Caio Molinaro <cmolinaro19@gmail.com>
2024-03-13 18:41:50 -0300

    feat: Created Basket Microsservice

---
+++ Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,38 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class BasketController : ControllerBase
+{
+    private readonly IBasketRepository _repository;
+
+    public BasketController(IBasketRepository repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpGet("{userName}", Name = "GetBasketAsync")]
+    public async Task<ActionResult<ShoppingCart>> GetBasketAsync(string userName)
+    {
+        var basket = await _repository.GetBasketAsync(userName);
+
+        return Ok(basket ?? new ShoppingCart(userName));
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ShoppingCart>> UpdateBasketAsync([FromBody] ShoppingCart basket)
+    {
+        return Ok(await _repository.UpdateBasketAsync(basket));
+    }
+
+    [HttpDelete("{userName}", Name = "DeleteBasketAsync")]
+    public async Task<IActionResult> DeleteBasketAsync(string userName)
+    {
+        await _repository.DeleteBasketAsync(userName);
+        return Ok();
+    }
+}
---
+++ Basket.API/Program.cs
@@ -0,0 +1,34 @@
+using Basket.API.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var Configuration = builder.Configuration;
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionString");
+});
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
